c2d43f048f2a65133ea7c5226b7a36e0956a56f9
Severah <severahmwanyumba24@gmail.com>
2024-06-19 11:26:46 +0300

    Portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('contactForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+        alert('Thank you for your message!');
+    });
+});
